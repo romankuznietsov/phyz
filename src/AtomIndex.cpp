b96148219ce5012894b556e228f7e20c26fcc3b4
Roman Kuznietsov <roman.kuznietsov@gmail.com>
2012-11-21 20:30:49 +0200

    implement add and update methods in atom index

--- src/AtomIndex.cpp
+++ src/AtomIndex.cpp
@@ -1,18 +1,25 @@
 #include "AtomIndex.h"
 
 
-AtomIndex::AtomIndex()
+AtomIndex::AtomIndex(float atomRadius) :
+	_collisionDistance(atomRadius * 2.0f)
 {
 }
 
 
 void AtomIndex::add(unsigned int atom, Vector position)
 {
+	_xIndex[position.x].insert(atom);
+	_yIndex[position.y].insert(atom);
 }
 
 
 void AtomIndex::update(unsigned int atom, Vector oldPosition, Vector newPosition)
 {
+	_xIndex[oldPosition.x].erase(atom);
+	_yIndex[oldPosition.y].erase(atom);
+	_xIndex[newPosition.x].insert(atom);
+	_yIndex[newPosition.y].insert(atom);
 }
 
 
--- src/AtomIndex.h
+++ src/AtomIndex.h
@@ -13,11 +13,12 @@ typedef std::unordered_set<unsigned int> AtomSet;
 class AtomIndex
 {
 	private:
+		float _collisionDistance;
 		std::map<float, AtomSet> _xIndex;
 		std::map<float, AtomSet> _yIndex;
 
 	public:
-		AtomIndex();
+		AtomIndex(float atomRadius);
 		void add(unsigned int atom, Vector position);
 		void update(unsigned int atom, Vector oldPosition, Vector newPosition);
 		AtomSet near(unsigned int atom, Vector position);
