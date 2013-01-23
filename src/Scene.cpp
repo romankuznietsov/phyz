4cea6826a282d126b60de8be26d7147ba60c6789
Roman Kuznietsov <roman.kuznietsov@gmail.com>
2013-01-23 20:30:54 +0200

    add even more atoms to scene

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -9,8 +9,8 @@ Scene::Scene() :
 	_paused(true),
 	_lastDt(0.0f)
 {
-	_atoms.addBody(Vector(-180.0f, -400.0f), Vector(180.0f, 400.0f), Vector(), 10.0f);
-	_atoms.addBody(Vector(700.0f, 50.0f), Vector(600.0f, -50.0f), Vector(-1000.0f, 0.0f), 10.0f);
+	_atoms.addBody(Vector(-200.0f, -400.0f), Vector(200.0f, 400.0f), Vector(), 10.0f);
+	_atoms.addBody(Vector(900.0f, 50.0f), Vector(600.0f, -50.0f), Vector(-1000.0f, 0.0f), 10.0f);
 }
 
 
