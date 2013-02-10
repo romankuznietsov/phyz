9138694e19ab9fcd1c8e0c4e655a7703bd264419
Roman Kuznietsov <roman.kuznietsov@gmail.com>
2013-02-10 16:37:46 +0200

    use gxy

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,79 +1,23 @@
 #include "Application.h"
 #include <GL/freeglut.h>
-#include <stdio.h> // for demo
 
 
-Scene Application::_scene;
-const int updatePeriod = 10;
-int Application::_lastUpdateTime = 0;
-int Application::_width = 800;
-int Application::_height = 600;
+Application::Application() : _lastUpdateTime(0)
+{}
 
 
-void Application::exit()
-{
-    _scene.writeUsedTime();
-    glutLeaveMainLoop();
-}
-
-
-void Application::registerCallbacks()
-{
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
-    glutKeyboardUpFunc(keyboardUp);
-    glutMouseFunc(mouse);
-    glutMotionFunc(motion);
-    glutSpecialFunc(special);
-    glutSpecialUpFunc(specialUp);
-}
-
-void Application::run(int argc, char** argv)
-{
-    _scene.load(argc, argv);
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowPosition(0, 0);
-    glutInitWindowSize(_width, _height);
-    glutCreateWindow("app");
-
-    registerCallbacks();
-
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-_width / 2, _width / 2, -_height / 2, _height / 2, -1.0, 1.0);
-
-    glutTimerFunc(updatePeriod, update, 0);
-    glutMainLoop();
-}
-
-
-void Application::update(int i)
+void Application::update()
 {
     int elapsedTime = glutGet(GLUT_ELAPSED_TIME);
     float dt = (elapsedTime - _lastUpdateTime) / 1000.0f;
     _lastUpdateTime = elapsedTime;
-
-    // update
     _scene.update(dt);
-
-    glutTimerFunc(updatePeriod, update, 0);
-    glutPostRedisplay();
 }
 
 
 void Application::display()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glColor3f(1.0, 1.0, 1.0);
-
-    // draw
     _scene.draw(_width, _height);
-
-    glutSwapBuffers();
 }
 
 
@@ -81,12 +25,6 @@ void Application::reshape(int width, int height)
 {
     _width = width;
     _height = height;
-    glViewport(0, 0, (GLsizei) _width, (GLsizei) _height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-_width / 2, _width / 2, -_height / 2, _height / 2, -1.0, 1.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 }
 
 
@@ -95,7 +33,7 @@ void Application::keyboard(unsigned char key, int x, int y)
     switch(key)
     {
 	case 27:
-	    exit();
+	    quit();
 	    break;
 	case 'p':
 	    _scene.togglePause();
@@ -103,36 +41,4 @@ void Application::keyboard(unsigned char key, int x, int y)
 	default:
 	    break;
     }
-}
-
-
-void Application::keyboardUp(unsigned char key, int x, int y)
-{
-}
-
-
-void Application::mouse(int button, int state, int x, int y)
-{
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-    {
-	if (glutGetModifiers() & GLUT_ACTIVE_CTRL)
-	{
-	    _scene.addAtom(Vector(x - _width / 2, - y + _height / 2));
-	}
-    }
-}
-
-
-void Application::motion(int x, int y)
-{
-}
-
-
-void Application::special(int key, int x, int y)
-{
-}
-
-
-void Application::specialUp(int key, int x, int y)
-{
 }
--- src/Application.h
+++ src/Application.h
@@ -1,34 +1,24 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
-
+#include "gxy/Gxy.h"
 #include "Scene.h"
 
 
-class Application
+class Application : public Gxy::Application
 {
-	private:
-		static Scene _scene;
+    private:
+	Scene _scene;
+	int _width;
+	int _height;
+	int _lastUpdateTime;
+	void update();
+	void display();
+	void keyboard(unsigned char key, int x, int y);
+	void reshape(int width, int height);
 
-		static int _lastUpdateTime;
-		static int _width;
-		static int _height;
-
-		static void exit();
-		static void registerCallbacks();
-
-		static void update(int i);
-		static void display();
-		static void reshape(int w, int h);
-
-		static void keyboard(unsigned char key, int x, int y);
-		static void keyboardUp(unsigned char key, int x, int y);
-		static void mouse(int button, int state, int x, int y);
-		static void motion(int x, int y);
-		static void special(int key, int x, int y);
-		static void specialUp(int key, int x, int y);
-	public:
-		static void run(int argc, char** argv);
+    public:
+	Application();
 };
 
 
